1b2f85408d6eae355baf241dcfbd901ae6913c02
dumiheo <dumihi313@gmail.com>
2022-05-28 20:33:03 +0700

    Done 2

--- app/src/main/java/vn/metamon/core/Configs.kt
+++ app/src/main/java/vn/metamon/core/Configs.kt
@@ -3,44 +3,22 @@ package vn.metamon.core
 import vn.metamon.BuildConfig
 
 data class Configs(
-    val streamServer: StreamServer,
     val showStarTab: Boolean,
     val rankingUrl: String,
-    val userDiamondDetailUrl: String,
-    val classifyMissionUrl: String,
     val currentVersion: String,
     val lowestVersion: String,
-    val dailyMissionUrl: String? = null,
-    val idolOnboardingUrl: String? = null,
     val agencyDashboardUrl: String? = null,
-    val csUrl: String? = null,
-    val levelUrl: String? = null,
-    val attractivePointUrl: String? = null,
-    val starDetailUrl: String? = null,
-    val userBagUrl: String? = null,
     val eventUrl: String? = null,
-    val eventIconUrl: String? = null,
-    val userMissionUrl: String? = null,
-    val userMissionIconUrl: String? = null,
 ) {
     companion object {
         @JvmStatic
         val DEFAULT = Configs(
-            StreamServer(
-                BuildConfig.DEFAULT_STREAM_HOST,
-                BuildConfig.DEFAULT_STREAM_PORT
-            ),
             false,
             BuildConfig.DEFAULT_URL,
-            BuildConfig.DEFAULT_URL,
-            BuildConfig.DEFAULT_URL,
             BuildConfig.VERSION_NAME,
-            BuildConfig.VERSION_NAME
+            BuildConfig.VERSION_NAME,
+            BuildConfig.DEFAULT_URL,
+            BuildConfig.DEFAULT_URL,
         )
     }
-
-    data class StreamServer(
-        val host: String,
-        val port: Int
-    )
 }
